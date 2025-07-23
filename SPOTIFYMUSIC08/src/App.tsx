954c8ad6edb42d5d17ddc07c8469a340b96dabb4
Ayush <ayushedu684@gmail.com>
2025-07-23 18:54:23 +0530

    add some changes in app

---
+++ SPOTIFYMUSIC08/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+
+function App() {
+
+
+  return (
+   <></>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
